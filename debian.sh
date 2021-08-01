41d2c968b669d55b5d6836e92caecc4e0d590c67
Patton <48503044+Beurling@users.noreply.github.com>
2021-08-01 17:11:45 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,1 +1,13 @@
+cd /root
+apt-get install ibus-libpinyin net-tools network-manager network-manager-gnome -y
 
+cat /etc/NetworkManager/NetworkManager.conf
+
+cd /root
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+apt-get update -y
+apt-get install google-chrome-stable -y
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e "\033[32m Chrome Already installed  is [OK] \033[0m"
+echo "---------------------------------------------------------------------------------------------------------------------"
